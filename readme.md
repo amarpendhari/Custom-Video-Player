89dfbe8b58f75242b5d89dafc976e36f74775f31
Amar Pendhari <amar.pendhari97@gmail.com>
2020-06-22 23:08:15 +0530

    Commiting Video Player to Git

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Custom Video Player
+
+Custom Video Player to play Videos
+
+## Project Specifications
+
+- Display Video controls when hover on Player
+- On Click Player, Play or Pause video also icon change
+- Display + Controlable video progress bar
+- Volume & Playback Rate can be controled with range sliders
+- Back 10s + Forward 25s button implementation
+- FullScreen Mode btn
---
+++ script.js
@@ -0,0 +1,85 @@
+
+      /* FullScreen
+      let x = screen.width ;
+      let y = screen.height ;
+      const fullscreen = document.getElementById('fullscreen');
+      fullscreen.addEventListener('click', () => {
+      $('.player').css({"width": "1360px","height": "786px"}) })*/
+        var elem = document.getElementById("myvideo");
+        function openFullscreen() {
+              if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+              } else if (elem.mozRequestFullScreen) { 
+                elem.mozRequestFullScreen();
+              } else if (elem.webkitRequestFullscreen) { 
+                elem.webkitRequestFullscreen();
+              } else if (elem.msRequestFullscreen) { 
+                elem.msRequestFullscreen();
+              }
+        }
+    /*Get our Elements*/
+      const player = document.querySelector('.player');
+      const video = player.querySelector('.viewer');     
+      const progress = player.querySelector('.progress');     
+      const progressBar = player.querySelector('.progress__filled');     
+      const toggle = player.querySelector('.toggle');     
+      const skipButtuons = player.querySelectorAll('[data-skip]');     
+      const ranges = player.querySelectorAll('.player__slider'); 
+      
+    /*build out functions*/
+      function togglePlay(){
+          const method = video.paused ? 'play' : 'pause';
+          video[method]();
+          /*if(video.paused) {
+              video.play();
+          }else {
+              video.pause();
+          }*/
+      }
+  
+      function updateButton(){
+         const icon = this.paused ? '►' : '❚ ❚';
+        console.log(icon);
+        toggle.textContent = icon;
+      }
+      
+      function skip(){
+          console.log(this.dataset);
+          video.currentTime += parseFloat(this.dataset.skip);
+      }
+     function handleRangeUpdate(){
+         video[this.name] = this.value;
+         console.log(this.value)
+     }
+      
+    function handleProgress() {
+        const percent = (video.currentTime / video.duration) * 100;
+        progressBar.style.flexBasis = `${percent}%`;
+    }
+      
+    function scrub(e) {
+        const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
+       console.log(scrubTime);
+    }
+/*Hook up the event Listners*/
+      video.addEventListener('click', togglePlay);
+      video.addEventListener('play', updateButton);
+      video.addEventListener('pause', updateButton);
+      video.addEventListener('timeupdate', handleProgress);  // timeupdate or progress
+    
+      toggle.addEventListener('click', togglePlay);
+      skipButtuons.forEach(button => button.addEventListener('click', skip));
+      ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+      ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+      
+      let mousedown = false;
+      progress.addEventListener('click', scrub);
+      /*progress.addEventListener('mousemove', () => {
+          if(mousedown){
+            scrub();
+          }
+      });*/
+      progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+      progress.addEventListener('mousedown', () => mousedown = true);
+      progress.addEventListener('mouseup', () => mousedown = false);
